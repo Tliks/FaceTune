f3f061dba6b1333eb07ca4d1cc4b938824ae5e1b
tliks <aoft725@gmail.com>
2025-05-21 20:55:17 +0900

    chore: FaceTuneCustomEditorBase for ExpressionPattern

---
+++ Editor/GUI/Inspector/CustomEditor/ExpressionPattern/PatternEditor.cs
@@ -0,0 +1,7 @@
+namespace com.aoyon.facetune.ui;
+
+[CanEditMultipleObjects]
+[CustomEditor(typeof(PatternComponent))]
+internal class PatternEditor : FaceTuneCustomEditorBase<PatternComponent>
+{
+}
