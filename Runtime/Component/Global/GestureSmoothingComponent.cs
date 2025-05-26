2d785b79abd215afe094c227c2b28278cbcd06d5
tliks <aoft725@gmail.com>
2025-05-26 22:14:23 +0900

    chore: rename

--- Runtime/Component/Global/GestureSmoothingComponent.cs
+++ Runtime/Component/Global/GestureSmoothingComponent.cs
@@ -1,9 +1,9 @@
 namespace com.aoyon.facetune
 {
     [AddComponentMenu(MenuPath)]
-    public class HaiGestureSmoothingComponent : FaceTuneTagComponent
+    public class GestureSmoothingComponent : FaceTuneTagComponent
     {
         internal const string MenuPath = FaceTune + "/" + Global + "/" + ComponentName;
-        internal const string ComponentName = "FT Hai Gesture Smoothing";
+        internal const string ComponentName = "FT Gesture Smoothing";
     }
 }   
