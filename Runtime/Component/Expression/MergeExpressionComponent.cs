56acaf82a4b35bebfb6f15433de89c1e1a758686
tliks <aoft725@gmail.com>
2025-06-19 22:35:42 +0900

    fix: MergeExpressionComponent

--- Runtime/Component/Expression/MergeExpressionComponent.cs
+++ Runtime/Component/Expression/MergeExpressionComponent.cs
@@ -1,34 +1,33 @@
-namespace com.aoyon.facetune;
-
-[DisallowMultipleComponent]
-[AddComponentMenu(MenuPath)]
-public class MergeExpressionComponent : ExpressionComponentBase
+namespace com.aoyon.facetune
 {
-    internal const string ComponentName = "FT Merge Expression";
-    internal const string MenuPath = FaceTune + "/" + Expression + "/" + ComponentName;
-
-    public FacialSettings FacialSettings = new(); // 影響下にFacialExpressionComponentがある場合のみ有効
-
-    internal Expression? mergedExpression = null;
-
-    internal override Expression ToExpression(SessionContext sessionContext, IObserveContext observeContext)
+    [AddComponentMenu(MenuPath)]
+    public class MergeExpressionComponent : ExpressionComponentBase
     {
-        mergedExpression ??= Merge(sessionContext);
-        return mergedExpression;
-    }
+        internal const string ComponentName = "FT Merge Expression";
+        internal const string MenuPath = FaceTune + "/" + Expression + "/" + ComponentName;
 
-    internal Expression Merge(SessionContext sessionContext)
-    {
-        var facialSettings = gameObject.GetComponentsInChildren<FacialExpressionComponent>(true).Any() ? FacialSettings : FacialSettings.Keep;
-        var mergedExpression = new Expression(name, new List<GenericAnimation>(), ExpressionSettings, facialSettings);
-        var expressionComponents = gameObject.GetComponentsInChildren<ExpressionComponentBase>(true);
-        var nonObserveContext = new NonObserveContext();
-        foreach (var expressionComponent in expressionComponents)
+        public FacialSettings FacialSettings = new(); // 影響下にFacialExpressionComponentがある場合のみ有効
+
+        internal override Expression ToExpression(SessionContext sessionContext, IObserveContext observeContext)
         {
-            var expression = expressionComponent.ToExpression(sessionContext, nonObserveContext);
-            expression.MergeAnimation(expression.Animations);
-            Object.DestroyImmediate(expressionComponent);
+            return Merge(sessionContext);
         }
-        return mergedExpression;
+
+        internal Expression Merge(SessionContext sessionContext)
+        {
+            var facialSettings = gameObject.GetComponentsInChildren<FacialExpressionComponent>(true).Any() ? FacialSettings : FacialSettings.Keep;
+            var mergedExpression = new Expression(name, new List<GenericAnimation>(), ExpressionSettings, facialSettings);
+            var expressionComponents = gameObject.GetComponentsInChildren<ExpressionComponentBase>(true);
+            var nonObserveContext = new NonObserveContext();
+            foreach (var expressionComponent in expressionComponents)
+            {
+                if (expressionComponent == this) continue;
+
+                var expression = expressionComponent.ToExpression(sessionContext, nonObserveContext);
+                mergedExpression.MergeAnimation(expression.Animations);
+                Object.DestroyImmediate(expressionComponent);
+            }
+            return mergedExpression;
+        }
     }
 }
