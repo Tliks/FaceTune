b483257cad9c4f13dc07114d82dabbf4da5b7368
tliks <aoft725@gmail.com>
2025-06-30 10:07:01 +0900

    wip

--- Editor/GUI/CustomEditor/CutomEditorUtility.cs
+++ Editor/GUI/CustomEditor/CutomEditorUtility.cs
@@ -24,4 +24,50 @@ internal static class CustomEditorUtility
         if (string.IsNullOrEmpty(path)) return;
         AssetDatabase.CreateAsset(clip, path);
     }
+
+    public static void OpenEditor(GameObject obj, Action<BlendShapeSet> onApply)
+    {
+        if (!TryGetContext(obj, out var context)) return;
+        var window = FacialShapesEditor.OpenEditor(context.FaceRenderer, context.FaceMesh, new(context.ZeroWeightBlendShapes), new());
+        if (window == null) return;
+        window.RegisterApplyCallback(onApply);
+    }
+
+    public static void ClearAnimations(SerializedProperty property)
+    {
+        property.arraySize = 0;
+    }
+
+    public static void AddShapesAsSingleFrame(SerializedProperty property, IReadOnlyList<BlendShape> newShapes, bool clear = false)
+    {
+        var animations = newShapes.Select(shape => BlendShapeAnimation.SingleFrame(shape.Name, shape.Weight)).ToList();
+        if (clear)
+        {
+            ClearAnimations(property);
+        }
+        property.arraySize = animations.Count;
+        for (var i = 0; i < animations.Count; i++)
+        {
+            var element = property.GetArrayElementAtIndex(i);
+            var nameProp = element.FindPropertyRelative(BlendShapeAnimation.NamePropName);
+            var curveProp = element.FindPropertyRelative(BlendShapeAnimation.CurvePropName);
+            nameProp.stringValue = animations[i].Name;
+            curveProp.animationCurveValue = animations[i].Curve;
+        }
+    }
+
+    public static void AddAnimations(SerializedProperty property, IReadOnlyList<BlendShapeAnimation> newAnimations, bool clear = false)
+    {
+        if (clear)
+        {
+            ClearAnimations(property);
+        }
+        property.arraySize = newAnimations.Count;
+        for (var i = 0; i < newAnimations.Count; i++)
+        {
+            var element = property.GetArrayElementAtIndex(i);
+            element.FindPropertyRelative(BlendShapeAnimation.NamePropName).stringValue = newAnimations[i].Name;
+            element.FindPropertyRelative(BlendShapeAnimation.CurvePropName).animationCurveValue = newAnimations[i].Curve;
+        }
+    }
 }
