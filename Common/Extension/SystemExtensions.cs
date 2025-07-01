789502aad32d07f44013a3a95de333f8f4c03a48
tliks <aoft725@gmail.com>
2025-07-01 17:05:51 +0900

    fix: GetSequenceHashCode

--- Common/Extension/SystemExtensions.cs
+++ Common/Extension/SystemExtensions.cs
@@ -4,11 +4,12 @@ namespace aoyon.facetune;
 
 internal static class SystemExtensions
 {
-    public static int GetSequenceHashCode<T>(this IEnumerable<T> sequence) where T : notnull
+    public static int GetSequenceHashCode<T>(this IEnumerable<T> sequence)
     {
         var hash = 0;
         foreach (var item in sequence)
         {
+            if (item == null) continue;
             hash ^= item.GetHashCode();
         }
         return hash;
