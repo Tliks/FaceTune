32d6678b8cc880d1e96a61e6df1a39a67c5c3513
tliks <aoft725@gmail.com>
2025-07-06 21:37:49 +0900

    EnableEyeBlinkInAllChildExpressions

--- Editor/GUI/CustomEditor/Tracking/AdvancedEyeBlinkEditor.cs
+++ Editor/GUI/CustomEditor/Tracking/AdvancedEyeBlinkEditor.cs
@@ -14,5 +14,22 @@ internal class AdvancedEyeBlinkEditor : FaceTuneCustomEditorBase<AdvancedEyeBlin
             var getProperty = (SerializedObject so) => so.FindProperty(nameof(AdvancedEyeBlinkComponent.AdvancedEyeBlinkSettings)).FindPropertyRelative(AdvancedEyeBlinkSettings.CancelerBlendShapeNamesPropName);
             CustomEditorUtility.OpenEditorAndApplyBlendShapeNames(Component, defaultOverride, getProperty);
         }
+        if (GUILayout.Button("Enable Eye Blink in All Child Expressions"))
+        {
+            EnableEyeBlinkInAllChildExpressions();
+        }
+    }
+
+    private void EnableEyeBlinkInAllChildExpressions()
+    {
+        var expressions = Component.GetComponentsInChildren<ExpressionComponent>();
+        if (expressions.Length == 0) return;
+
+        Undo.RecordObjects(expressions, "Enable Eye Blink in All Child Expressions");
+        foreach (var expression in expressions)
+        {
+            expression.FacialSettings = expression.FacialSettings with { AllowEyeBlink = TrackingPermission.Allow };
+        }
+        Debug.Log($"Enabled Eye Blink in {expressions.Length} child expressions");
     }
 }
