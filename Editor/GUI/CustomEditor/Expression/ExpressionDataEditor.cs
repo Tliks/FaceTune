1477d052105f39d5bd9b7ac63ab0bc13d18c94d0
tliks <aoft725@gmail.com>
2026-01-09 19:19:48 +0900

    refactor

--- Editor/GUI/CustomEditor/Expression/ExpressionDataEditor.cs
+++ Editor/GUI/CustomEditor/Expression/ExpressionDataEditor.cs
@@ -135,16 +135,18 @@ internal class ExpressionDataEditor : FaceTuneIMGUIEditorBase<ExpressionDataComp
     private void OpenEditor()
     {
         if (!CustomEditorUtility.TryGetContext(Component.gameObject, out var context)) throw new InvalidOperationException("Context not found");
+
         var bodyPath = context.BodyPath;
+
         var facialStyleAnimations = new List<BlendShapeWeightAnimation>();
         FacialStyleContext.TryGetFacialStyleAnimations(Component.gameObject, facialStyleAnimations);
-
-        var defaultOverride = new BlendShapeWeightSet();
-        defaultOverride.AddRange(Component.BlendShapeAnimations.ToFirstFrameBlendShapes());
 
         var baseSet = new BlendShapeWeightSet();
         baseSet.AddRange(facialStyleAnimations.ToFirstFrameBlendShapes());
         baseSet.AddRange(Component.ProcessClip(bodyPath).facialAnimations.ToFirstFrameBlendShapes());
+
+        var defaultOverride = new BlendShapeWeightSet();
+        defaultOverride.AddRange(Component.BlendShapeAnimations.ToFirstFrameBlendShapes());
 
         CustomEditorUtility.OpenEditor(Component.gameObject, new ExpressionDataTargeting(){ Target = Component }, defaultOverride, baseSet);
     }
